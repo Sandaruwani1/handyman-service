dc123afa3d09f7ca25491ec63b979df4d3709a74
sandaruwani1 <sandaruwaniw14@gmail.com>
2023-02-23 20:16:46 +0530

    first commit

---
+++ src/main/java/com/sandaruwani/handymanservice/HandymanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sandaruwani.handymanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandymanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandymanServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandaruwani/handymanservice/HandymanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandaruwani.handymanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
